0da5905c9a2f7d03660e6d3b07aa0207eafa1c35
proffatai <proffatai@gmail.com>
2022-05-21 19:06:28 +0100

    Added alternative way to find element on a webpage

--- src/main/java/SeleniumRunner.java
+++ src/main/java/SeleniumRunner.java
@@ -16,6 +16,11 @@ public class SeleniumRunner {
         //that the search bar on github can be accessed via the css selector name='q. Now we have to pass it as an argument
         //to the By.cssSelector() in ""
 
+        //We can also use the below code instead of line 15, they give same result. Below, we accessed the search bar via the
+        //the class name of the search bar
+        // WebElement searchInput= driver.findElement(By.cssSelector("input.form-control.input-sm.header-search-input.jump-to-field.js-jump-to-field.js-site-search-focus"));
+
+
         //What we just did is that, we just automated our script to identify the seach bar
         searchInput.sendKeys("gitstart"); // Now we just entered gitstart into the search bar
         searchInput.sendKeys(Keys.ENTER); // Now we pressed the enter button

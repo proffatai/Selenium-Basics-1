d20690c3a6fac5878551665f023bd35a1ecbf5c1
proffatai <proffatai@gmail.com>
2022-05-21 19:03:55 +0100

    Junit included

--- src/main/java/SeleniumRunner.java
+++ src/main/java/SeleniumRunner.java
@@ -5,24 +5,28 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
 public class SeleniumRunner {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup(); // this specifies that the webdriver we will be using in our project is chromedriver
         WebDriver driver = new ChromeDriver(); // we instantiated a chromedriver, i.e we created an object of chromedriver. We could have created a firefox or edge driver
         driver.manage   ().window().maximize(); // we maximized the window of our browser
-        driver.get("https://www.amazon.com"); //we open the amazon homepage, which is the entry point of our program
+        driver.get("https://www.github.com"); //we open the amazon homepage, which is the entry point of our program
 
-        WebElement searchInput= driver.findElement(By.cssSelector("#twotabsearchtextbox")); // with the aid of chroPath, we figured
-        //that the search bar on amazon can be accessed via the css selector #twotabsearchtextbox. Now we have to pass it as an argument
-        //to the By.cssSelector() in ""
+        WebElement searchInput= driver.findElement(By.cssSelector("input.form-control.input-sm.header-search-input.jump-to-field.js-jump-to-field.js-site-search-focus"));
 
-        //What we just did is that, we just automated our script to identify the seach bar
-        searchInput.sendKeys("iphone"); // Now we just entered iphone into the search bar
+        String repoName="Selenium";
+        searchInput.sendKeys(repoName); // Now we just entered iphone into the search bar
         searchInput.sendKeys(Keys.ENTER); // Now we pressed the enter button
 
+       // List<String> repos =driver.findElements(By.cssSelector(".repo-list-item")).stream()
+              //  .map(element ->element.getText().toLowerCase()).collect(Collectors.toList());
 
-
-
+       // Assert.assertEqual
+        //System.out.println(repos);
         //driver.quit(); // To automatically close the window
     }
 }

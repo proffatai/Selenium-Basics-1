91b886c256a722929fd611900b191bbfa6c91351
proffatai <proffatai@gmail.com>
2022-05-21 12:01:09 +0100

    automated search for iphone on amazon

--- src/main/java/SeleniumRunner.java
+++ src/main/java/SeleniumRunner.java
@@ -9,15 +9,15 @@ public class SeleniumRunner {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup(); // this specifies that the webdriver we will be using in our project is chromedriver
         WebDriver driver = new ChromeDriver(); // we instantiated a chromedriver, i.e we created an object of chromedriver. We could have created a firefox or edge driver
-        driver.manage().window().maximize(); // we maximized the window of our browser
-        driver.get("https://www.github.com"); //we open the github homepage, which is the entry point of our program
+        driver.manage   ().window().maximize(); // we maximized the window of our browser
+        driver.get("https://www.amazon.com"); //we open the amazon homepage, which is the entry point of our program
 
-        WebElement searchInput= driver.findElement(By.cssSelector("[name='q']")); // with the aid of chroPath, we figured
-        //that the search bar on github can be accessed via the css selector name='q. Now we have to pass it as an argument
+        WebElement searchInput= driver.findElement(By.cssSelector("#twotabsearchtextbox")); // with the aid of chroPath, we figured
+        //that the search bar on amazon can be accessed via the css selector #twotabsearchtextbox. Now we have to pass it as an argument
         //to the By.cssSelector() in ""
 
         //What we just did is that, we just automated our script to identify the seach bar
-        searchInput.sendKeys("gitstart"); // Now we just entered gitstart into the search bar
+        searchInput.sendKeys("iphone"); // Now we just entered iphone into the search bar
         searchInput.sendKeys(Keys.ENTER); // Now we pressed the enter button
 
         //driver.quit(); // To automatically close the window
